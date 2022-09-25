c89f8125bddd545f0b97229006d8736487d88cc6
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-25 14:28:18 +0300

    Add files via upload

---
+++ zodyak.java
@@ -0,0 +1,69 @@
+package zodyak;
+import java.util.Scanner;
+public class zodyak {
+
+	public static void main(String[] args) {
+		int yil,kalan;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Doğum yılınızı giriniz:");
+		yil=scanner.nextInt();
+		kalan=yil%12;
+		
+		switch(kalan) {
+		case 1:
+			{System.out.println("HOROZ");
+			break;
+			}
+		case 2:
+			{System.out.println("KÖPEK");
+			break;
+			}
+		case 3:
+			{System.out.println("DOMUZ");
+			break;
+			}
+		
+		case 4:
+			{System.out.println("FARE");
+			break;
+			}
+		case 5:
+			{System.out.println("ÖKÜZ");
+			break;
+			}
+		case 6:
+			{System.out.println("KAPLAN");
+			break;
+			}
+			
+		case 7:
+			{System.out.println("TAVŞAN");
+			break;
+			}
+		case 8:
+			{System.out.println("EJDERHA");
+			break;
+			}
+		case 9:
+			{System.out.println("YILAN");
+			break;
+			}
+		case 10:
+			{System.out.println("1AT");
+			break;
+			}
+		case 11:
+			{System.out.println("KOYUN");
+			break;
+			}
+		case 0:
+			{System.out.println("MAYMUN");
+			break;
+			}
+			
+			
+		}
+
+	}
+
+}
